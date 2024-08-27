874f55c62ced9c21268bbee41e54362725bf4394
Hofor <127219361+Hofor@users.noreply.github.com>
2024-08-27 14:47:19 +0200

    Add files via upload

---
+++ ScriptPad/Online_Windows23H2.ps1
@@ -0,0 +1,125 @@
+Invoke-Expression -Command (Invoke-RestMethod -Uri functions.osdcloud.com)
+
+
+$Manufacturer = (Get-CimInstance -Class:Win32_ComputerSystem).Manufacturer
+$Model = (Get-CimInstance -Class:Win32_ComputerSystem).Model
+$HPTPM = $false
+$HPBIOS = $false
+$HPIADrivers = $false
+
+if ($Manufacturer -match "HP" -or $Manufacturer -match "Hewlett-Packard"){
+    $Manufacturer = "HP"
+    if ($InternetConnection){
+        $HPEnterprise = Test-HPIASupport
+    }
+}
+if ($HPEnterprise){
+    Invoke-Expression (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/OSDeploy/OSD/master/cloud/modules/deviceshp.psm1')
+    osdcloud-InstallModuleHPCMSL
+    $TPM = osdcloud-HPTPMDetermine
+    $BIOS = osdcloud-HPBIOSDetermine
+    $HPIADrivers = $true
+    if ($TPM){
+    write-host "HP Update TPM Firmware: $TPM - Requires Interaction" -ForegroundColor Yellow
+        $HPTPM = $true
+    }
+    Else {
+        $HPTPM = $false
+    }
+
+    if ($BIOS -eq $false){
+        $CurrentVer = Get-HPBIOSVersion
+        write-host "HP System Firmware already Current: $CurrentVer" -ForegroundColor Green
+        $HPBIOS = $false
+    }
+    else
+        {
+        $LatestVer = (Get-HPBIOSUpdates -Latest).ver
+        $CurrentVer = Get-HPBIOSVersion
+        write-host "HP Update System Firmwware from $CurrentVer to $LatestVer" -ForegroundColor Yellow
+        $HPBIOS = $true
+    }
+}
+
+#Variables to define the Windows OS / Edition etc to be applied during OSDCloud
+$Product = (Get-MyComputerProduct)
+$Model = (Get-MyComputerModel)
+$Manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
+$OSVersion = 'Windows 11' #Used to Determine Driver Pack
+$OSReleaseID = '23H2' #Used to Determine Driver Pack
+$OSName = 'Windows 11 23H2 x64'
+$OSEdition = 'Enterprise'
+$OSActivation = 'Volume'
+$OSLanguage = 'da-dk'
+
+
+#Set OSDCloud Vars
+$Global:MyOSDCloud = [ordered]@{
+    Restart = [bool]$False
+    RecoveryPartition = [bool]$true
+    OEMActivation = [bool]$True
+    WindowsUpdate = [bool]$true
+    WindowsUpdateDrivers = [bool]$true
+    WindowsDefenderUpdate = [bool]$true
+    SetTimeZone = [bool]$true
+    ClearDiskConfirm = [bool]$False
+	NetFx3 = [bool]$True
+    ShutdownSetupComplete = [bool]$false
+    SyncMSUpCatDriverUSB = [bool]$true
+    CheckSHA1 = [bool]$true
+}
+
+#Testing MS Update Catalog Driver Sync
+#$Global:MyOSDCloud.DriverPackName = 'Microsoft Update Catalog'
+
+#Used to Determine Driver Pack
+$DriverPack = Get-OSDCloudDriverPack -Product $Product -OSVersion $OSVersion -OSReleaseID $OSReleaseID
+
+if ($DriverPack){
+    $Global:MyOSDCloud.DriverPackName = $DriverPack.Name
+}
+
+<#If Drivers are expanded on the USB Drive, disable installing a Driver Pack
+if (Test-DISMFromOSDCloudUSB -eq $true){
+    Write-Host "Found Driver Pack Extracted on Cloud USB Flash Drive, disabling Driver Download via OSDCloud" -ForegroundColor Green
+    if ($Global:MyOSDCloud.SyncMSUpCatDriverUSB -eq $true){
+        write-host "Setting DriverPackName to 'Microsoft Update Catalog'"
+        $Global:MyOSDCloud.DriverPackName = 'Microsoft Update Catalog'
+    }
+    else {
+        write-host "Setting DriverPackName to 'None'"
+        $Global:MyOSDCloud.DriverPackName = "None"
+    }
+}
+#>
+#Enable HPIA | Update HP BIOS | Update HP TPM
+ 
+if (Test-HPIASupport){
+    #$Global:MyOSDCloud.DevMode = [bool]$True
+    $Global:MyOSDCloud.HPTPMUpdate = [bool]$True
+    if ($Product -ne '83B2' -or $Model -notmatch "zbook"){$Global:MyOSDCloud.HPIAALL = [bool]$true} #I've had issues with this device and HPIA
+    #{$Global:MyOSDCloud.HPIAALL = [bool]$true}
+    $Global:MyOSDCloud.HPBIOSUpdate = [bool]$true
+    $Global:MyOSDCloud.HPCMSLDriverPackLatest = [bool]$true #In Test 
+    #Set HP BIOS Settings to what I want:
+    #iex (irm https://raw.githubusercontent.com/gwblok/garytown/master/OSD/CloudOSD/Manage-HPBiosSettings.ps1)
+    #Manage-HPBiosSettings -SetSettings
+}
+
+if ($Manufacturer -match "Lenovo") {
+    #Set Lenovo BIOS Settings to what I want:
+    #iex (irm https://raw.githubusercontent.com/gwblok/garytown/master/OSD/CloudOSD/Manage-LenovoBiosSettings.ps1)
+    #Manage-LenovoBIOSSettings -SetSettings
+}
+
+#write variables to console
+Write-Output $Global:MyOSDCloud
+
+#Launch OSDCloud
+Write-Host "Starting OSDCloud" -ForegroundColor Green
+write-host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage"
+
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
+
+#Restart
+restart-computer
