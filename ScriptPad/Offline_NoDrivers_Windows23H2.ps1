8ecf720cfc90d7c9318606a9ad358beec191adb0
Hofor <127219361+Hofor@users.noreply.github.com>
2024-10-02 10:53:12 +0200

    Add files via upload

---
+++ ScriptPad/Offline_NoDrivers_Windows23H2.ps1
@@ -0,0 +1,47 @@
+﻿
+Invoke-Expression -Command (Invoke-RestMethod -Uri functions.osdcloud.com)
+
+$Manufacturer = (Get-CimInstance -Class:Win32_ComputerSystem).Manufacturer
+$Model = (Get-CimInstance -Class:Win32_ComputerSystem).Model
+
+#Variables to define the Windows OS / Edition etc to be applied during OSDCloud
+$Product = (Get-MyComputerProduct)
+$Model = (Get-MyComputerModel)
+$Manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
+$OSVersion = 'Windows 11' #Used to Determine Driver Pack
+$OSReleaseID = '23H2' #Used to Determine Driver Pack
+$OSName = 'Windows 11 23H2 x64'
+$OSEdition = 'Enterprise'
+$OSActivation = 'Volume'
+$OSLanguage = 'da-dk'
+
+
+#Set OSDCloud Vars
+$Global:MyOSDCloud = [ordered]@{
+    Restart = [bool]$False
+    RecoveryPartition = [bool]$true
+    OEMActivation = [bool]$True
+    WindowsUpdate = [bool]$False
+    WindowsUpdateDrivers = [bool]$False
+    WindowsDefenderUpdate = [bool]$False
+    SetTimeZone = [bool]$true
+    ClearDiskConfirm = [bool]$False
+    NetFx3 = [bool]$false
+    ShutdownSetupComplete = [bool]$false
+    SyncMSUpCatDriverUSB = [bool]$false
+    CheckSHA1 = [bool]$true
+}
+
+#write variables to console
+Write-Output $Global:MyOSDCloud
+
+#Launch OSDCloud
+Write-Host "Starting OSDCloud" -ForegroundColor Green
+write-host "Start-OSDCloud -FindImageFile -OSimageIndex 1 -ZTI"
+
+Start-OSDCloud -FindImageFile -OSimageIndex 1 -ZTI
+
+write-host "OSDCloud Process Complete, Running Custom Actions From Script Before Reboot" -ForegroundColor Green
+
+#Restart
+restart-computer
