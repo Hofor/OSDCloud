c09acf4989241e3b105867ed4bdfcf0350f0be31
Hofor <127219361+Hofor@users.noreply.github.com>
2024-10-02 10:27:14 +0200

    Add files via upload

---
+++ ScriptPad/Online_Lenovo_Windows23H2.ps1
@@ -0,0 +1,109 @@
+﻿Invoke-Expression -Command (Invoke-RestMethod -Uri functions.osdcloud.com)
+
+$Manufacturer = (Get-CimInstance -Class:Win32_ComputerSystem).Manufacturer
+$Model = (Get-CimInstance -Class:Win32_ComputerSystem).Model
+
+#Variables to define the Windows OS / Edition etc to be applied during OSDCloud
+$Product = (Get-MyComputerProduct)
+$Model = (Get-MyComputerModel)
+$Manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
+$OSVersion = 'Windows 11' #Used to Determine Driver Pack
+$OSReleaseID = '23H2' #Used to Determine Driver Pack
+$OSName = 'Windows 11 23H2 x64'
+$OSEdition = 'Enterprise'
+$OSActivation = 'Volume'
+$OSLanguage = 'da-dk'
+
+#Set OSDCloud Vars
+$Global:MyOSDCloud = [ordered]@{
+    Restart = [bool]$False
+    RecoveryPartition = [bool]$true
+    OEMActivation = [bool]$True
+    WindowsUpdate = [bool]$true
+    WindowsUpdateDrivers = [bool]$true
+    WindowsDefenderUpdate = [bool]$true
+    SetTimeZone = [bool]$true
+    ClearDiskConfirm = [bool]$False
+    NetFx3 = [bool]$True
+    ShutdownSetupComplete = [bool]$false
+    SyncMSUpCatDriverUSB = [bool]$true
+    CheckSHA1 = [bool]$true
+}
+
+#Testing MS Update Catalog Driver Sync
+#$Global:MyOSDCloud.DriverPackName = 'Microsoft Update Catalog'
+
+<# 
+Used to Determine Driver Pack - OSDCloud will natively do this, so you don't have to, but..
+I want to control exactly how drivers are being done, what I'm doing here is..
+- Search for Driver Pack, if found, populate the driver pack variable information used in OSDCloud
+- Check to see if I have driver packs already downloaded and extracted into the DISM folder on the OSDCloudUSB
+  - If I do, Check if I'm wanting to Sync the MS Update Catalog drivers to the USB (Set above), because then I assume I want it to use the MS Catalog to suppliment my own drivers
+  - If I do want to sync, set the OSDCloud driver pack variables to use the Microsoft Update Catalog
+  - if I don't, set the driver pack to none, so it will ONLY use the drivers I have extracted into my DISM folder on the OSDCloudUSB
+#>
+#Region Determine if using native driver packs, or if I want to use extracted drivers on OSDCloudUSB
+$Product = (Get-MyComputerProduct)
+$DriverPack = Get-OSDCloudDriverPack -Product $Product -OSVersion $OSVersion -OSReleaseID $OSReleaseID
+
+if ($DriverPack){
+    $Global:MyOSDCloud.DriverPackName = $DriverPack.Name
+}
+
+write-host $Global:MyOSDCloud.DriverPackName
+
+#If Drivers are expanded on the USB Drive, disable installing a Driver Pack
+if ((Test-DISMFromOSDCloudUSB) -eq $true){
+    Write-Host "Found Driver Pack Extracted on Cloud USB Flash Drive, disabling Driver Download via OSDCloud" -ForegroundColor Green
+    $Global:MyOSDCloud.DriverPackName = "None"
+}
+else
+{
+    #if ($Global:MyOSDCloud.SyncMSUpCatDriverUSB -eq $true) {
+        $Global:MyOSDCloud.DriverPackName = 'Microsoft Update Catalog'   
+    #}
+}
+#endregion Driver Pack Stuff
+
+#write variables to console
+Write-Output $Global:MyOSDCloud
+
+$Manufacturer = (Get-CimInstance -Namespace root/CIMV2 -ClassName Win32_ComputerSystem).Manufacturer
+Write-Output "Manufacturer = $Manufacturer"
+if ($Manufacturer -match "Lenovo"){
+    iex (irm https://raw.githubusercontent.com/OSDeploy/OSD/master/Public/OSDCloudTS/Install-LenovoApps.ps1)
+    iex (irm https://sandbox.osdcloud.com)
+    Install-LenovoVantage
+    Set-LenovoVantage
+    Write-Host "Device is Lenovo, attempting to install Module LSUClient"
+    Install-Module -Name LSUClient -Force
+    Import-Module -Name LSUClient
+    Write-Host "Scanning for updates...."
+    $updates = Get-LSUpdate | Where-Object { $_.Installer.Unattended }
+    $i = 1
+    foreach ($update in $updates) {
+        Write-Host "Installing update $i of $($updates.Count): $($update.Title)"
+        Install-LSUpdate -Package $update -Verbose
+        $i++
+    }
+
+    $LenovoBackgroundTask = Get-ScheduledTask -TaskName "Background monitor" -ErrorAction SilentlyContinue
+    if ($LenovoBackgroundTask){
+        $LenovoBackgroundTask | Disable-ScheduledTask 
+    }
+}
+
+#Launch OSDCloud
+Write-Host "Starting OSDCloud" -ForegroundColor Green
+write-host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage"
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage -SkipAutopilot -ZTI
+
+<#This is now native in OSDCloud
+write-host "OSDCloud Process Complete, Running Custom Actions Before Reboot" -ForegroundColor Green
+if (Test-DISMFromOSDCloudUSB){
+    Start-DISMFromOSDCloudUSB
+}
+#>
+
+#Restart Computer from WInPE into Full OS to continue Process
+restart-computer
