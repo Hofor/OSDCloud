5c6a76bf36ed33016e9a7c22426929c7781da7ea
Hofor <127219361+Hofor@users.noreply.github.com>
2025-10-28 14:18:07 +0100

    Update 3_SRO_Windows25H2.ps1

--- ScriptPad/3_SRO_Windows25H2.ps1
+++ ScriptPad/3_SRO_Windows25H2.ps1
@@ -65,6 +65,9 @@ restart-computer
 write-host "Invoke Copy Start-OSDCloud.windeploy.specialize.ps1"
 Invoke-WebRequest 'https://raw.githubusercontent.com/Hofor/OSDCloud/refs/heads/main/SRO/Start-OSDCloud.windeploy.specialize.ps1' -OutFile "C:\Windows\System32\OOBE\Start-OSDCloud.windeploy.specialize.ps1"
 
+write-host "Invoke Copy Start-OOBEDeploy.ps1"
+Invoke-WebRequest 'https://raw.githubusercontent.com/Hofor/OSDCloud/refs/heads/main/SRO/Start-OOBEDeploy.ps1' -OutFile "C:\Windows\System32\OOBE\Start-OOBEDeploy.ps1"
+
 write-host "Set Content In unattend.xml"
 Invoke-WebRequest 'https://raw.githubusercontent.com/Hofor/OSDCloud/refs/heads/main/SRO/unattend.xml' -OutFile "C:\Windows\Panther\unattend.xml"
 
