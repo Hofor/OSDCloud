ff9567be5ba5a644f6dd230a754da155aae6ef83
Hofor <127219361+Hofor@users.noreply.github.com>
2025-10-31 13:42:29 +0100

    Update 3_SRO_Windows25H2.ps1

--- ScriptPad/3_SRO_Windows25H2.ps1
+++ ScriptPad/3_SRO_Windows25H2.ps1
@@ -60,8 +60,20 @@ write-host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $
 Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage -SkipAutopilot -ZTI
 
 #Restart Computer from WInPE into Full OS to continue Process
-restart-computer
 
+
+write-host "Invoke Copy Start-OSDCloud.windeploy.specialize.ps1"
+Invoke-WebRequest 'https://raw.githubusercontent.com/Hofor/OSDCloud/refs/heads/main/SRO/Start-OSDCloud.windeploy.specialize.ps1' -OutFile "C:\Windows\System32\OOBE\Start-OSDCloud.windeploy.specialize.ps1"
+
+write-host "Invoke Copy Start-OOBEDeploy.ps1"
+Invoke-WebRequest 'https://raw.githubusercontent.com/Hofor/OSDCloud/refs/heads/main/SRO/Start-OOBEDeploy.ps1' -OutFile "C:\Windows\System32\OOBE\Start-OOBEDeploy.ps1"
+
+write-host "Set Content In unattend.xml"
+Invoke-WebRequest 'https://raw.githubusercontent.com/Hofor/OSDCloud/refs/heads/main/SRO/unattend.xml' -OutFile "C:\Windows\Panther\unattend.xml"
+
+write-host "Start-OSDCloud.windeploy.specialize"
+
+restart-computer
 write-host "Invoke Copy Start-OSDCloud.windeploy.specialize.ps1"
 Invoke-WebRequest 'https://raw.githubusercontent.com/Hofor/OSDCloud/refs/heads/main/SRO/Start-OSDCloud.windeploy.specialize.ps1' -OutFile "C:\Windows\System32\OOBE\Start-OSDCloud.windeploy.specialize.ps1"
 
