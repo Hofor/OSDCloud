b6399c80e1422adeabbe7c5199b638a6acd5d597
Hofor <127219361+Hofor@users.noreply.github.com>
2025-11-03 13:56:54 +0100

    Update Start-OSDCloud.windeploy.specialize.ps1

--- SRO/Start-OSDCloud.windeploy.specialize.ps1
+++ SRO/Start-OSDCloud.windeploy.specialize.ps1
@@ -7,20 +7,137 @@ Write-Host "OSDCloud Specialize Configuration Starting..." -ForegroundColor Cyan
 
 # 1. Sæt computernavn
 $Serial = Get-WmiObject Win32_bios | Select-Object -ExpandProperty SerialNumber
-$NewComputerName = "SRO-$($Serial)"
+$NewComputerName = "SRO-FJV-$($Serial)"
 Write-Host "Renaming computer to $NewComputerName"
 (Get-WmiObject Win32_ComputerSystem).Rename($NewComputerName)
 
-# 2. Opret lokal administratorbruger
-#$Username = "SRO-Admin"
-#$Password = ConvertTo-SecureString "SRO-ADMIN123456" -AsPlainText -Force
-#New-LocalUser -Name $Username -Password $Password -FullName "SRO Local Admin" -Description "Local Admin"
-#Add-LocalGroupMember -Group "Administratorer" -Member $Username
-#Write-Host "Local admin user '$Username' created"
+# 4. Fjern uønskede Windows Capabilities
+Write-Host "Fjern uønskede Windows Capabilities" -ForegroundColor Green
+
+$CapabilitiesToRemove = @(
+    "OpenSSH.Client~~~~0.0.1.0",
+    "XPS.Viewer~~~~0.0.1.0",
+    "Microsoft.Windows.WordPad~~~~0.0.1.0",
+	"OneCoreUAP.OneSync~~~~0.0.1.0",
+	"Print.Management.Console~~~~0.0.1.0",
+	"VBSCRIPT~~~~",
+	"Media.WindowsMediaPlayer~~~~0.0.12.0",
+	"Microsoft.Windows.PowerShell.ISE",
+	"Microsoft.Wallpapers.Extended",
+	"MathRecognizer~~~~0.0.1.0",
+	"Language.TextToSpeech~~~da-DK~0.0.1.0",
+	"App.StepsRecorder~~~~0.0.1.0",
+	"Browser.InternetExplorer~~~~0.0.11.0",
+	"MathRecognizer~~~~0.0.1.0"
+)
+
+foreach ($cap in $CapabilitiesToRemove) {
+    Write-Host "Removing capability: $cap"
+    Remove-WindowsCapability -Online -Name $cap
+}
+
+# 5. Fjern uønskede Inbox Apps
+$AppsToRemove = @(
+		"MSTeams",
+        "MicrosoftTeams",
+        "Microsoft.BingWeather",
+        "Microsoft.BingNews",
+        "Microsoft.GamingApp",
+        "Microsoft.GetHelp",
+        "Microsoft.Getstarted",
+        "Microsoft.Messaging",
+        "Microsoft.MicrosoftOfficeHub",
+        "Microsoft.MicrosoftSolitaireCollection",
+        "Microsoft.MicrosoftStickyNotes",
+        "Microsoft.MSPaint",
+        "Microsoft.People",
+        "Microsoft.PowerAutomateDesktop",
+        "Microsoft.StorePurchaseApp",
+        "Microsoft.Todos",
+        "microsoft.windowscommunicationsapps",
+        "Microsoft.WindowsFeedbackHub",
+        "Microsoft.WindowsMaps",
+        "Microsoft.WindowsSoundRecorder",
+        "Microsoft.Xbox.TCUI",
+        "Microsoft.XboxGameOverlay",
+        "Microsoft.XboxGamingOverlay",
+        "Microsoft.XboxIdentityProvider",
+        "Microsoft.XboxSpeechToTextOverlay",
+        "Microsoft.YourPhone",
+        "Microsoft.ZuneMusic",
+        "Microsoft.ZuneVideo",
+	    "Microsoft.XboxApp",
+        "Microsoft.OneNote",
+        "Microsoft.MicrosoftSolitaireCollection",
+		"AppUp.ThunderboltControlCenter",
+		"DolbyLaboratories.DolbyAccess",
+		"DolbyLaboratories.DolbyDigitalPlusDecoderOEM",
+		"Microsoft.BingSearch",
+		"Microsoft.Edge.GameAssist",
+		"Microsoft.MicrosoftEdge.Stable",
+		"Microsoft.OutlookForWindows",
+		"Microsoft.OutlookForWindows_1.0.0.0_neutral__8wekyb3d8bbwe",
+		"Microsoft.Paint",
+		"Microsoft.Windows.DevHome",
+		"Microsoft.Windows.Photos",
+		"Microsoft.WindowsAlarms",
+		"Microsoft.WindowsCalculator",
+		"Microsoft.WindowsNotepad",
+		"Microsoft.WindowsStore",
+		"Microsoft.WindowsTerminal",
+		"MicrosoftCorporationII.QuickAssist",
+		"MicrosoftWindows.Client.WebExperience"
+)
+
+Write-Host "Fjern uønskede Inbox Apps" -ForegroundColor Green
+
+foreach ($app in $AppsToRemove) {
+    Write-Host "Removing app: $app"
+    Get-AppxPackage -Name $app | Remove-AppxPackage
+    Get-AppxProvisionedPackage -Online | Where-Object DisplayName -EQ $app | Remove-AppxProvisionedPackage -Online
+}
 
 # 3. Sæt netværksprofil til privat
-#Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
-#Write-Host "Network profile set to Private"
+Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
+Write-Host "Network profile set to Private"
+
+Set-NetFirewallProfile -Profile Domain, Public -Enabled False
+
+# Åbn outbound TCP port 3389 for privat profil
+New-NetFirewallRule -DisplayName "Allow Outbound TCP 3389 - Private" -Direction Outbound -Protocol TCP -LocalPort 3389 -Action Allow -Profile Private
+
+# Åbn outbound UDP port 3389 for privat profil
+New-NetFirewallRule -DisplayName "Allow Outbound UDP 3389 - Private" -Direction Outbound -Protocol UDP -LocalPort 3389 -Action Allow -Profile Private
+
+# Åbn outbound TCP port 3850 for privat profil
+New-NetFirewallRule -DisplayName "Allow Outbound TCP 3850 - Private" -Direction Outbound -Protocol TCP -LocalPort 3850 -Action Allow -Profile Private
+
+# Åbn outbound TCP port 3851 for privat profil
+New-NetFirewallRule -DisplayName "Allow Outbound TCP 3851 - Private" -Direction Outbound -Protocol TCP -LocalPort 3851 -Action Allow -Profile Private
+
+#Åben for WSUS
+# Tillad inbound TCP trafik på port 8530 for Private profil
+New-NetFirewallRule -DisplayName "Allow Inbound WSUS 10.209.148.16 TCP 8530 - Private" -Direction Inbound -Protocol TCP -LocalPort 8530 -Action Allow -Profile Private
+
+# Tillad outbound TCP trafik på port 8530 for Private profil
+New-NetFirewallRule -DisplayName "Allow Outbound WSUS to 10.209.148.16 TCP 8530" -Direction Outbound -Protocol TCP -RemoteAddress 10.209.148.16 -RemotePort 8530 -Action Allow -Profile Private
+
+Write-Host "Specialize configuration. Proceeding to OOBE..." -ForegroundColor Green
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name WUServer -Value 'http://10.209.148.16:8530'
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name WUStatusServer -Value 'http://10.209.148.16:8530'
+
+Write-Host "Set MAK Key..." -ForegroundColor Green
+
+# Erstat XXXXX-XXXXX-XXXXX-XXXXX-XXXXX med din MAK-nøgle
+$MakKey = "G2NRJ-VRWY2-8PVPR-R6T46-V6DF4"
+
+Set-location -path c:\windows\system32
+
+# Sæt produktnøglen
+slmgr.vbs /ipk $MakKey
+
+# Aktivér Windows
+slmgr.vbs /ato
 
 # 6. Forbered til OOBE
 <#
