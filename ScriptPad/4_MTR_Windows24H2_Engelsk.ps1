d904c4f0cb66f71a9c25852658e8a4da9151e092
Hofor <127219361+Hofor@users.noreply.github.com>
2025-12-11 14:17:19 +0100

    Add files via upload

---
+++ ScriptPad/4_MTR_Windows24H2_Engelsk.ps1
@@ -0,0 +1,71 @@
+Invoke-Expression -Command (Invoke-RestMethod -Uri functions.osdcloud.com)
+
+$Manufacturer = (Get-CimInstance -Class:Win32_ComputerSystem).Manufacturer
+$Model = (Get-CimInstance -Class:Win32_ComputerSystem).Model
+
+#Variables to define the Windows OS / Edition etc to be applied during OSDCloud
+$Product = (Get-MyComputerProduct)
+$Model = (Get-MyComputerModel)
+$Manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
+$OSVersion = 'Windows 11' #Used to Determine Driver Pack
+$OSReleaseID = '24H2' #Used to Determine Driver Pack
+$OSName = 'Windows 11 24H2 x64'
+$OSEdition = 'Enterprise'
+$OSActivation = 'Volume'
+$OSLanguage = 'en-US'
+
+#Set OSDCloud Vars
+$Global:MyOSDCloud = [ordered]@{
+    Restart = [bool]$False
+    RecoveryPartition = [bool]$true
+    OEMActivation = [bool]$True
+    WindowsUpdate = [bool]$true
+    WindowsUpdateDrivers = [bool]$true
+    WindowsDefenderUpdate = [bool]$true
+    SetTimeZone = [bool]$true
+    ClearDiskConfirm = [bool]$False
+    NetFx3 = [bool]$True
+    ShutdownSetupComplete = [bool]$false
+    SyncMSUpCatDriverUSB = [bool]$true
+    CheckSHA1 = [bool]$true
+}
+
+#Region Determine if using native driver packs, or if I want to use extracted drivers on OSDCloudUSB
+$Product = (Get-MyComputerProduct)
+
+write-host "Product"
+write-host $Product
+
+$DriverPack = Get-OSDCloudDriverPack -Product $Product -OSVersion $OSVersion -OSReleaseID $OSReleaseID
+
+write-host "DriverPack"
+write-host $DriverPack 
+
+if ($DriverPack){
+    $Global:MyOSDCloud.DriverPackName = $DriverPack.Name
+}
+
+write-host $Global:MyOSDCloud.DriverPackName
+
+#If Drivers are expanded on the USB Drive, disable installing a Driver Pack
+if ((Test-DISMFromOSDCloudUSB) -eq $true){
+    Write-Host "Found Driver Pack Extracted on Cloud USB Flash Drive, disabling Driver Download via OSDCloud" -ForegroundColor Green
+    $Global:MyOSDCloud.DriverPackName = "None"
+}
+else
+{
+   $Global:MyOSDCloud.DriverPackName = 'Microsoft Update Catalog'  
+}
+#endregion Driver Pack Stuff
+
+#write variables to console
+Write-Output $Global:MyOSDCloud
+
+#Launch OSDCloud
+Write-Host "Starting OSDCloud" -ForegroundColor Green
+write-host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage"
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage -SkipAutopilot -ZTI
+
+
+#Restart Computer from WInPE into Full OS to continue Process
+restart-computer
