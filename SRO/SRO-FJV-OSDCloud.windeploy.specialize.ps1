217a657eb28a89f4a357d3dc1a911395d81b0ecf
AndreasKleissl <andkle@hofor.dk>
2026-06-24 14:36:48 +0200

    Update SRO-FJV-OSDCloud.windeploy.specialize.ps1

--- SRO/SRO-FJV-OSDCloud.windeploy.specialize.ps1
+++ SRO/SRO-FJV-OSDCloud.windeploy.specialize.ps1
@@ -203,10 +203,6 @@ $RdpPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\R
 Set-ItemProperty -Path $RdpPath -Name "UserAuthentication" -Value 0
 
 # (Valgfrit, men ofte nødvendigt) Sørg for RDP er aktiveret
-#Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
-
-# Genstart RDP-service
-#Restart-Service -Name TermService -Force
-
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
 
 Stop-Transcript
