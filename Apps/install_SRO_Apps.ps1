d428181cb06b6b060f860cbaee341af51646e979
Hofor <127219361+Hofor@users.noreply.github.com>
2026-06-25 12:42:51 +0200

    Update install_SRO_Apps.ps1

--- Apps/install_SRO_Apps.ps1
+++ Apps/install_SRO_Apps.ps1
@@ -1,6 +1,13 @@
+$Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-AppInstall-Script.log"
+Start-Transcript -Path (Join-Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" $Global:Transcript) -ErrorAction Ignore
+
+Write-Host "Execute OSD Cloud App Install Script" -ForegroundColor Green
+
 $usb = Get-Volume | Where-Object { Test-Path "$($_.DriveLetter):\OSDCloud\Apps\ADSelfServicePlusClientSoftware.msi" } | Select-Object -First 1
 
 $msi = "$($usb.DriveLetter):\OSDCloud\Apps\ADSelfServicePlusClientSoftware.msi"
 $mst = "$($usb.DriveLetter):\OSDCloud\Apps\ADSelfServicePlusClientSoftware.mst"
 
 Start-Process msiexec.exe -ArgumentList "/i `"$msi`" TRANSFORMS=`"$mst`" /qn /norestart" -Wait -NoNewWind -PassThru
+
+Stop-Transcript
