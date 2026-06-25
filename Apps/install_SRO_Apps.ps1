50c55a0f4a7c69868cae79cc7d63a675f39528a6
Hofor <127219361+Hofor@users.noreply.github.com>
2026-06-25 11:06:02 +0200

    Update install_SRO_Apps.ps1

--- Apps/install_SRO_Apps.ps1
+++ Apps/install_SRO_Apps.ps1
@@ -1,10 +1,6 @@
-
 $usb = Get-Volume | Where-Object { Test-Path "$($_.DriveLetter):\OSDCloud\Apps\ADSelfServicePlusClientSoftware.msi" } | Select-Object -First 1
 
 $msi = "$($usb.DriveLetter):\OSDCloud\Apps\ADSelfServicePlusClientSoftware.msi"
 $mst = "$($usb.DriveLetter):\OSDCloud\Apps\ADSelfServicePlusClientSoftware.mst"
 
 Start-Process msiexec.exe -ArgumentList "/i `"$msi`" TRANSFORMS=`"$mst`" /qn /norestart" -Wait -NoNewWind -PassThru
-
-
-
