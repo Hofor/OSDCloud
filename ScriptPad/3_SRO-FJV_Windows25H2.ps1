c1f0f4a12f9484026811449c2a565eb64581e851
AndreasKleissl <andkle@hofor.dk>
2026-06-29 15:22:05 +0200

    Update 3_SRO-FJV_Windows25H2.ps1

--- ScriptPad/3_SRO-FJV_Windows25H2.ps1
+++ ScriptPad/3_SRO-FJV_Windows25H2.ps1
@@ -29,6 +29,9 @@ $Global:MyOSDCloud = [ordered]@{
     ShutdownSetupComplete = [bool]$false
     SyncMSUpCatDriverUSB = [bool]$true
     CheckSHA1 = [bool]$true
+    CopyToWindows = @{
+        "C:\OSDCache" = "OSDCloud\Apps"
+    }
 }
 
 #Region Determine if using native driver packs, or if I want to use extracted drivers on OSDCloudUSB
