c833f0b4dd0366c7dde1e4633fa9f5d6e33fb894
Hofor <127219361+Hofor@users.noreply.github.com>
2026-07-24 12:34:45 +0200

    Add files via upload

---
+++ scripts/removeAppx.ps1
@@ -0,0 +1,44 @@
+﻿Write-Host "Removing unwanted AppX packages..." -ForegroundColor Cyan
+
+$AppxPackages = @(
+    "Microsoft.BingWeather",
+    "Microsoft.BingNews",
+    "Microsoft.GamingApp",
+    "Microsoft.GetHelp",
+    "Microsoft.Getstarted",
+    "Microsoft.MicrosoftOfficeHub",
+    "Microsoft.MicrosoftSolitaireCollection",
+    "Microsoft.People",
+    "Microsoft.PowerAutomateDesktop",
+    "Microsoft.Todos",
+    "Microsoft.WindowsFeedbackHub",
+    "Microsoft.WindowsMaps",
+    "Microsoft.WindowsSoundRecorder",
+    "Microsoft.Xbox.TCUI",
+    "Microsoft.XboxGameOverlay",
+    "Microsoft.XboxGamingOverlay",
+    "Microsoft.XboxIdentityProvider",
+    "Microsoft.XboxSpeechToTextOverlay",
+    "Microsoft.YourPhone",
+    "Microsoft.ZuneMusic",
+    "Microsoft.ZuneVideo",
+    "MicrosoftTeams",
+    "MSTeams",
+    "Clipchamp.Clipchamp",
+    "MicrosoftCorporationII.QuickAssist",
+    "Microsoft.OutlookForWindows",
+    "Microsoft.Windows.DevHome"
+
+)
+
+foreach ($Package in $AppxPackages) {
+
+    Write-Host "Removing $Package"
+
+    Get-AppxPackage -AllUsers -Name $Package -ErrorAction SilentlyContinue |
+        Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue
+
+    Get-AppxProvisionedPackage -Online |
+        Where-Object DisplayName -eq $Package |
+        Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
+}
