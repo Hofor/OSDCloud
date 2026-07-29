3f59c6665bb1031e6502b92d1c7fafdc0a84e994
Hofor <127219361+Hofor@users.noreply.github.com>
2026-07-29 14:39:16 +0200

    Update 1_Online_Windows24H2_new.ps1

--- ScriptPad/1_Online_Windows24H2_new.ps1
+++ ScriptPad/1_Online_Windows24H2_new.ps1
@@ -156,20 +156,66 @@ Write-Host ($Params | Out-String)
 Start-OSDCloud @Params
 #endregion
 
-<#
-$OSReleaseID = '24H2' #Used to Determine Driver Pack
-$OSName = 'Windows 11 24H2 x64'
-$OSEdition = 'Enterprise'
-$OSActivation = 'Volume'
-$OSLanguage = 'da-dk'
-$ZTI = $true
-$Firmware = $true
+#================================================
+#  [PostOS] OOBEDeploy Configuration
+#================================================
 
-#Launch OSDCloud
-Write-Host "Starting OSDCloud" -ForegroundColor Green
-write-host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage"
-Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage -ZTI $ZTI
-#>
+Write-Host -ForegroundColor Green "Create C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json"
+
+$OOBEDeployJson = @'
+{
+    "AddNetFX3": {
+        "IsPresent": true
+    },
+    "Autopilot": {
+        "IsPresent": true
+    },
+    "UpdateDrivers": {
+        "IsPresent": true
+    },
+    "UpdateWindows": {
+        "IsPresent": true
+    },
+    "UpdateDefender": {
+        "IsPresent": true
+    },
+    "RemoveAppx": [
+        "MSTeams",
+        "MicrosoftTeams",
+        "Microsoft.BingWeather",
+        "Microsoft.BingNews",
+        "Microsoft.GamingApp",
+        "Microsoft.GetHelp",
+        "Microsoft.Getstarted",
+        "Microsoft.Messaging",
+        "Microsoft.MicrosoftOfficeHub",
+        "Microsoft.MicrosoftSolitaireCollection",
+        "Microsoft.People",
+        "Microsoft.PowerAutomateDesktop",
+        "Microsoft.StorePurchaseApp",
+        "Microsoft.Todos",
+        "microsoft.windowscommunicationsapps",
+        "Microsoft.WindowsFeedbackHub",
+        "Microsoft.WindowsMaps",
+        "Microsoft.WindowsSoundRecorder",
+        "Microsoft.Xbox.TCUI",
+        "Microsoft.XboxGameOverlay",
+        "Microsoft.XboxGamingOverlay",
+        "Microsoft.XboxIdentityProvider",
+        "Microsoft.XboxSpeechToTextOverlay",
+        "Microsoft.YourPhone",
+        "Microsoft.ZuneMusic",
+        "Microsoft.ZuneVideo"
+    ]
+}
+'@
+
+If (!(Test-Path "C:\ProgramData\OSDeploy")) {
+    New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force | Out-Null
+}
+
+$OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json" -Encoding ascii -Force
+#endregion
 #=========================================================================
 # SetupComplete
 #=========================================================================
@@ -182,7 +228,7 @@ if (-not (Test-Path 'C:\Windows\Setup\Scripts')) {
 $SetupCompleteCMD = @'
 @echo off
 
-powershell.exe -ExecutionPolicy Bypass -Command "Invoke-Expression (Invoke-RestMethod ''https://raw.githubusercontent.com/Hofor/OSDCloud/main/scripts/removeAppx.ps1'')"
+#powershell.exe -ExecutionPolicy Bypass -Command "Invoke-Expression (Invoke-RestMethod ''https://raw.githubusercontent.com/Hofor/OSDCloud/main/scripts/removeAppx.ps1'')"
 
 powershell.exe -ExecutionPolicy Bypass -Command "Invoke-Expression (Invoke-RestMethod ''https://raw.githubusercontent.com/Hofor/OSDCloud/main/scripts/cleanupOSD.ps1'')"
 
