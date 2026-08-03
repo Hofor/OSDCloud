ac56d2cf8da8305070c9055b0c1fba26e3316931
Hofor <127219361+Hofor@users.noreply.github.com>
2026-08-03 13:23:53 +0200

    Add files via upload

--- ScriptPad/test.ps1
+++ ScriptPad/test.ps1
@@ -1,4 +1,18 @@
-﻿Invoke-Expression -Command (Invoke-RestMethod -Uri functions.osdcloud.com)
+﻿#Transport Layer Security (TLS) 1.2
+Write-Host -ForegroundColor Green "Transport Layer Security (TLS) 1.2"
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
+try {
+    Install-Module OSD -Force -SkipPublisherCheck
+}
+catch {
+    Write-Warning "OSD Module already installed or installation failed"
+}
+
+Import-Module OSD -Force
+
+Write-Host "Loading OSDCloud functions..." -ForegroundColor Green
+Invoke-Expression (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/OSDeploy/OSD/master/cloud/functions.ps1')
 
 $Manufacturer = (Get-CimInstance -Class:Win32_ComputerSystem).Manufacturer
 $Model = (Get-CimInstance -Class:Win32_ComputerSystem).Model
@@ -7,10 +21,28 @@ $Model = (Get-CimInstance -Class:Win32_ComputerSystem).Model
 $Product = (Get-MyComputerProduct)
 $Model = (Get-MyComputerModel)
 $Manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
+$OSVersion = 'Windows 11' #Used to Determine Driver Pack
+$OSReleaseID = '24H2' #Used to Determine Driver Pack
+$OSName = 'Windows 11 24H2 x64'
+$OSEdition = 'Enterprise'
+$OSActivation = 'Volume'
+$OSLanguage = 'da-dk'
+
+$DriverPack = Get-OSDCloudDriverPack -Product $Product -OSVersion $OSVersion -OSReleaseID $OSReleaseID
+if ($DriverPack) {
+    #$DriverPackName = $DriverPack.Name
+    $DriverPackName = 'Microsoft Update Catalog'
+    Write-Host "Matched driver pack: $DriverPackName" -ForegroundColor Green
+}
+else {
+    $DriverPackName = 'Microsoft Update Catalog'
+    Write-Warning "No OSDCloud driver pack matched product '$Product'. Using Microsoft Update Catalog drivers."
+}
+
 
 #Set OSDCloud Vars
 $Global:MyOSDCloud = [ordered]@{
-    Restart = [bool]$False
+    Restart = [bool]$True
     RecoveryPartition = [bool]$true
     OEMActivation = [bool]$True
     WindowsUpdate = [bool]$true
@@ -23,6 +55,9 @@ $Global:MyOSDCloud = [ordered]@{
     ShutdownSetupComplete = [bool]$false
     SyncMSUpCatDriverUSB = [bool]$false
     CheckSHA1 = [bool]$true
+    Product = $Product
+    DriverPackName = $DriverPackName
+
 }
 
 #=========================================================================
@@ -42,7 +77,7 @@ switch -Wildcard ($Manufacturer.ToUpper()) {
         $Global:MyOSDCloud.HPIAFirmware =  [bool]$true
 
         # Let OSDCloud determine HP driver package
-        $Global:MyOSDCloud.DriverPackName = $null
+        #$Global:MyOSDCloud.DriverPackName = $null
     }
 
     "*LENOVO*" {
@@ -50,15 +85,16 @@ switch -Wildcard ($Manufacturer.ToUpper()) {
         Write-Host "Lenovo Device detected" -ForegroundColor Green
 
         # Let OSDCloud select Lenovo OEM driver pack
-        $Global:MyOSDCloud.DriverPackName = $null
+        #$Global:MyOSDCloud.DriverPackName = $null
     }
 
     default {
+    
+    Write-Host "Using Microsoft Update Catalog drivers" -ForegroundColor Yellow
 
-        Write-Host "Using Microsoft Update Catalog drivers" -ForegroundColor Yellow
-
-        $Global:MyOSDCloud.DriverPackName   = 'Microsoft Update Catalog'
-        $Global:MyOSDCloud.MSCatalogFirmware = $true
+    #$Global:MyOSDCloud.DriverPackName = $null
+    #$Global:MyOSDCloud.WindowsUpdateDrivers = $true
+    $Global:MyOSDCloud.MSCatalogFirmware = $true
     }
 }
 
@@ -70,12 +106,7 @@ Write-Host ($Global:MyOSDCloud | Out-String)
 Write-Output "[OS] Params and Start-OSDCloud"
 #=======================================================================
 Write-Output "[OS] Start OSDCloud"
-$OSVersion = 'Windows 11' #Used to Determine Driver Pack
-$OSReleaseID = '24H2' #Used to Determine Driver Pack
-$OSName = 'Windows 11 24H2 x64'
-$OSEdition = 'Enterprise'
-$OSActivation = 'Volume'
-$OSLanguage = 'da-dk'
+
 Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage -SkipAutopilot -ZTI
 #endregion
 
