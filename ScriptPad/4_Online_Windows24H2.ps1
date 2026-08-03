35c1fb591cdbabeb1cb65eb872d8139d8419f472
Hofor <127219361+Hofor@users.noreply.github.com>
2026-08-03 15:23:33 +0200

    Update 4_Online_Windows24H2.ps1

--- ScriptPad/4_Online_Windows24H2.ps1
+++ ScriptPad/4_Online_Windows24H2.ps1
@@ -123,14 +123,22 @@ switch -Wildcard ($Manufacturer.ToUpper()) {
 
         Write-Host "HP Device Detected" -ForegroundColor Green
 
-        # HPIA styrer det hele
-        $Global:MyOSDCloud.DriverPackName = $null
-
-        $Global:MyOSDCloud.HPIADrivers  = $true
-        $Global:MyOSDCloud.HPIAFirmware = $true
-
-        $Global:MyOSDCloud.HPBIOSUpdate = $true
-        $Global:MyOSDCloud.HPTPMUpdate  = $true
+       if ($DriverPackName) {
+            $Global:MyOSDCloud.DriverPackName = $DriverPackName
+            $Global:MyOSDCloud.HPIADrivers  = $true
+        }
+        else {
+            $Global:MyOSDCloud.DriverPackName = 'Microsoft Update Catalog'
+            #$Global:MyOSDCloud.WindowsUpdate = $true
+            #$Global:MyOSDCloud.WindowsUpdateDrivers = $true
+            #$Global:MyOSDCloud.WindowsDefenderUpdate = $true
+            #$Global:MyOSDCloud.MSCatalogFirmware = $true
+        }
+        
+        
+        ##$Global:MyOSDCloud.HPIAFirmware = $true
+        ##$Global:MyOSDCloud.HPBIOSUpdate = $true
+        ##$Global:MyOSDCloud.HPTPMUpdate  = $true
     }
 
     "*LENOVO*" {
